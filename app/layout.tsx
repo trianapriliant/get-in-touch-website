1feb423e7b6506ab9d6d8a755d5e766aa480dbd5
trianapriliant <trianaprilianto3@gmail.com>
2025-04-27 10:29:32 +0700

    fix: bug on deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata = {
   title: 'Treean',
@@ -16,6 +17,7 @@ export default function RootLayout({
       <body>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
